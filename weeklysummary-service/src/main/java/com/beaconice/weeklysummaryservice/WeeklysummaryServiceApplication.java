097359ff77728802c3676022f8efe337c489a3ff
edwardng827 <edwardng827@gmail.com>
2020-04-04 14:26:39 -0400

    added services

---
+++ weeklysummary-service/src/main/java/com/beaconice/weeklysummaryservice/WeeklysummaryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.beaconice.weeklysummaryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeklysummaryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeeklysummaryServiceApplication.class, args);
+    }
+
+}
